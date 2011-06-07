45edf345593bcdcf6a94b204ffabe85491322169
Vanderson Mota <vanderson.mota@gmail.com>
2011-06-06 21:57:43 -0300

    file encryption/decryption implemented

--- lib/aes/aes.rb
+++ lib/aes/aes.rb
@@ -4,10 +4,22 @@ module AES
     def encrypt(plain_text, key, opts={})
       ::AES::AES.new(key, opts).encrypt(plain_text)
     end
+
+    # Encrypts an file
+    def encrypt_file(filepath, enc_filepath, key, opts={})
+      ::AES::AES.new(key, opts).encrypt_file(filepath, enc_filepath)
+    end
+
     # Decrypts the cipher_text with the provided key
     def decrypt(cipher_text, key, opts={})
       ::AES::AES.new(key, opts).decrypt(cipher_text)
     end
+
+    # Decrypts an file
+    def decrypt_file(filepath, enc_filepath, key, opts={})
+      ::AES::AES.new(key, opts).decrypt_file(filepath, enc_filepath)
+    end
+
     # Generates a random key of the specified length in bits
     # Default format is :plain
     def key(length=256,format=:plain)
@@ -19,7 +31,7 @@ module AES
         key
       end
     end
-    # Generates a random iv 
+    # Generates a random iv
     # Default format is :plain
     def iv(format=:plain)
       iv = ::AES::AES.new("").random_iv
@@ -28,10 +40,10 @@ module AES
         Base64.encode64(iv).chomp
       else
         iv
-      end      
+      end
     end
   end
-  
+
   class AES
     attr :options
     attr :key
@@ -39,7 +51,10 @@ module AES
     attr :cipher
     attr :cipher_text
     attr :plain_text
-  
+
+    ENCRYPT_CHUNK_SIZE = 2048
+    DECRYPT_CHUNK_SIZE = 2822 #it uses base64 on the encrypted chunk. this is the size after the base64 encoding.
+
     def initialize(key, opts={})
       merge_options opts
       @cipher = nil
@@ -47,7 +62,7 @@ module AES
       @iv   ||= random_iv
       self
     end
-  
+
     # Encrypts
     def encrypt(plain_text)
       @plain_text = plain_text
@@ -62,7 +77,18 @@ module AES
       @cipher_text
     end
 
-    # Decrypts  
+    def encrypt_file(filepath, encrypted_filepath)
+      enc_file = File.open(encrypted_filepath, 'wb')
+      File.open(filepath, 'rb') do |f|
+        until f.eof
+          chunk = f.read(ENCRYPT_CHUNK_SIZE)
+          enc_file.write(encrypt(chunk))
+        end
+      end
+      enc_file.close
+    end
+
+    # Decrypts
     def decrypt(cipher_text)
       @cipher_text = cipher_text
       _setup(:decrypt)
@@ -73,7 +99,18 @@ module AES
         ctext = @cipher_text
       end
       @cipher.iv  = ctext[0]
-      @plain_text = @cipher.update(ctext[1]) + @cipher.final 
+      @plain_text = @cipher.update(ctext[1]) + @cipher.final
+    end
+
+    def decrypt_file(encrypted_filepath, filepath)
+      file = File.open(filepath, 'wb')
+      File.open(encrypted_filepath, 'rb') do |f|
+        until f.eof
+          chunk = f.read(DECRYPT_CHUNK_SIZE)
+          file.write(decrypt(chunk))
+        end
+      end
+      file.close
     end
 
     # Generate a random initialization vector
@@ -81,24 +118,24 @@ module AES
       _setup(:encrypt)
       @cipher.random_iv
     end
-  
+
     # Generate a random key
     def random_key(length=256)
       _random_seed.unpack('H*')[0][0..((length/8)-1)]
     end
-  
+
     private
-    
+
       # Generates a random seed value
       def _random_seed(size=32)
         if defined? OpenSSL::Random
           return OpenSSL::Random.random_bytes(size)
         else
           chars = ("a".."z").to_a + ("A".."Z").to_a + ("0".."9").to_a
-          (1..size).collect{|a| chars[rand(chars.size)] }.join        
+          (1..size).collect{|a| chars[rand(chars.size)] }.join
         end
       end
-    
+
       # Un-Base64's the IV and CipherText
       # Returns an array containing the IV, and CipherText
       def b64_d(data)
@@ -108,12 +145,12 @@ module AES
         end
         iv_and_ctext
       end
-  
+
       # Base64 Encodes a string
       def b64_e(data)
         Base64.encode64(data).chomp
       end
-  
+
       # Encrypts @plain_text
       def _encrypt
         @cipher.update(@plain_text) + @cipher.final
@@ -128,7 +165,7 @@ module AES
         }.merge! opts
         _handle_iv
       end
-      
+
       def _handle_iv
         @iv = @options[:iv]
         return if @iv.nil?
@@ -138,10 +175,10 @@ module AES
           @iv  = Base64.decode64(@options[:iv])
         end
       end
-      
+
       # Create a new cipher using the cipher type specified
       def _setup(action)
-        @cipher ||= OpenSSL::Cipher::Cipher.new(@options[:cipher]) 
+        @cipher ||= OpenSSL::Cipher::Cipher.new(@options[:cipher])
         # Toggles encryption mode
         @cipher.send(action)
         @cipher.key = @key.unpack('a2'*32).map{|x| x.hex}.pack('c'*32)

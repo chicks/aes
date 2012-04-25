7c63a1400287063915d0300ae1329cacc26d708b
Alex Shulgin <ash@commandprompt.com>
2012-04-25 17:25:59 +0300

    Added :padding option.

--- test/test_aes.rb
+++ test/test_aes.rb
@@ -19,6 +19,17 @@ class TestAES < Test::Unit::TestCase
     enc2 = AES.encrypt(msg, key, {:iv => iv})
     assert_equal enc1, enc2
   end
+    
+  should "handle padding option" do
+    key = "01234567890123456789012345678901"
+    msg = "This is a message that nobody should ever see"
+    # unpadded message length should be a multiple of cipher block
+    # length (16 bytes)
+    msg += " "*(16 - (msg.length % 16))
+
+    enc = AES.encrypt(msg, key, {:padding => false})
+    assert_equal msg, AES.decrypt(enc, key, {:padding => false})
+  end
   
   should "generate a new key when AES#key" do
     assert_equal 32, AES.key.length
